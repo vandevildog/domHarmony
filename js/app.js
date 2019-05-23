ed7d4be5f98d4f23d0a8f9495206d971621aff7a
phi nguyen <vandevildog@gmail.comgit config --global user.email vandevildog@gmail.comgit commit -m 1st>
2019-05-22 20:50:22 -1000

    done up to question 4

--- js/app.js
+++ js/app.js
@@ -2,14 +2,21 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+matCount.innerHTML = 11;
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
 
+msgCount.innerHTML = 23;
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
+fullname.innerHTML = 'PEDO THE CLOWN';
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
 
+age.innerHTML = 63;
+
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
+job
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
